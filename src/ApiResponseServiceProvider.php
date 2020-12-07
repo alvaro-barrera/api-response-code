bdb24349c835cb32b7943dd0bebde06e1fc31ad4
alvaro <alvaro_bv@hotmail.com>
2020-12-07 11:00:22 -0500

    Depurando service provider

--- src/ApiResponseServiceProvider.php
+++ src/ApiResponseServiceProvider.php
@@ -9,24 +9,14 @@ class ApiResponseServiceProvider extends ServiceProvider
     public function boot()
     {
         if ($this->app->runningInConsole()) {
-//            $this->publishes([
-//                __DIR__ . '/../config/skeleton.php' => config_path('skeleton.php'),
-//            ], 'config');
-//
             $migrationFileName = '2020_12_05_113446_create_operation_responses_table.php';
-            if (! $this->migrationFileExists($migrationFileName)) {
+            if (!$this->migrationFileExists($migrationFileName)) {
                 $this->publishes([
                     __DIR__ . "/../database/migrations/{$migrationFileName}.stub" => database_path('migrations/' . date('Y_m_d_His', time()) . '_' . $migrationFileName),
                 ], 'migrations');
             }
             $this->loadMigrationsFrom(__DIR__ . '/../database/migrations/');
         }
-        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'skeleton');
-    }
-
-    public function register()
-    {
-        $this->mergeConfigFrom(__DIR__ . '/../config/skeleton.php', 'skeleton');
     }
 
     public static function migrationFileExists(string $migrationFileName): bool
@@ -37,7 +27,6 @@ class ApiResponseServiceProvider extends ServiceProvider
                 return true;
             }
         }
-
         return false;
     }
 }
